deb09bea3a16952813c0cc7bca7e20fe6d1f8a82
Bolu Aluko <alukoboluwatife3@gmail.com>
2021-08-20 17:20:53 +0100

    second

--- inventory/settings.py
+++ inventory/settings.py
@@ -118,7 +118,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Django Message Framework
 MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
